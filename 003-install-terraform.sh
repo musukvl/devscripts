9931de393d3ed83dc26f76bc9ce3207f991a98ee
Alexey Ryazhskikh <musukvl@gmail.com>
2023-03-17 14:54:52 +0200

    Create 003-install-terraform.sh

---
+++ 003-install-terraform.sh
@@ -0,0 +1,5 @@
+TFLINT_VERSION=$(curl -s https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep tag_name | cut -d '"' -f 4)
+wget "https://github.com/terraform-linters/tflint/releases/download/$TFLINT_VERSION/tflint_linux_amd64.zip" -O tflint.zip
+unzip tflint.zip
+chmod +x tflint
+mv tflint /usr/local/bin
