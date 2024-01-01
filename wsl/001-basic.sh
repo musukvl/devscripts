99f021ea79bf55dbf1199e020e3807f09afa9815
Alexey Ryazhskikh <musukvl@gmail.com>
2024-01-01 20:10:17 +0200

    Add git config

---
+++ wsl/001-basic.sh
@@ -0,0 +1,4 @@
+sudo apt-get install git -y
+git config --global user.name "Alexey Ryazhskikh"
+git config --global user.email "musukvl@gmail.com"
+git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
